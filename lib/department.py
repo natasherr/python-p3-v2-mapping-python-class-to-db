9f1a3dd970ca8144f841952e13300bfb9a8ac9f2
Ashley Natasha <ashleyotsiula@gmail.com>
2024-12-14 19:35:43 +0300

    finished

--- lib/department.py
+++ lib/department.py
@@ -10,3 +10,60 @@ class Department:
 
     def __repr__(self):
         return f"<Department {self.id}: {self.name}, {self.location}>"
+    
+    @classmethod
+    def create_table(cls):
+        """Create a new table to persist the attributes of Department instances"""
+        sql = """
+            CREATE TABLE IF NOT EXISTS departments(
+            id INTEGER PRIMARY KEY,
+            name TEXT,
+            location TEXT)
+            """
+        CURSOR.execute(sql)
+        CONN.commit()
+    
+    @classmethod
+    def drop_table(cls):
+        """Drop the table that persists Department instances"""
+        sql = """DROP TABLE IF EXISTS departments;"""
+
+        CURSOR.execute(sql)
+        CONN.commit()
+
+    def save(self):
+        sql = """
+            INSERT INTO departments (name, location)
+            VALUES (?, ?)
+            """
+        CURSOR.execute(sql, (self.name, self.location))
+        CONN.commit()
+
+        self.id = CURSOR.lastrowid
+
+    @classmethod
+    def create(cls, name, location):
+        department = cls(name, location)
+        department.save()
+        return department
+    
+    def update(self):
+        sql = """
+            UPDATE departments
+            SET name = ? , location = ? 
+            WHERE id = ?
+        """
+        CURSOR.execute(sql, (self.name, self.location, self.id))
+        CONN.commit()
+
+    def delete(self):
+            """Delete the table row corresponding to the current Department instance"""
+            sql = """
+                DELETE FROM departments
+                WHERE id = ?
+            """
+    
+            CURSOR.execute(sql, (self.id,))
+            CONN.commit()
+
+
